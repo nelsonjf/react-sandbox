f740af4f3501f84d4b9225465ff93f6fe9a28807
James <jfnelson2000@gmail.com>
2023-02-13 21:06:55 -0800

    Added WIP form to Counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 
 function Counter () {
     let [counter, setCounter] = useState(1)
+
     return (
         <div>
             <h1>{counter}</h1>
             <button onClick={() => setCounter(counter + 1)}>Add 1</button>
             <button onClick={() => setCounter(1)}>Reset</button>
-            <button onClick={()=> setCounter(counter - 1)}>Subtract 1</button>
+            <button onClick={()=> setCounter(counter - 1)}>Subtract 1</button><br/>
+            <form>
+                <input type="text" placeholder="NOT FUNCTIONAL"/>
+                <input type="submit" value="Set Value"/>
+            </form>
         </div>
     )
 
